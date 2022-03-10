585f2861d5d1122ca3943db6920cf520cda15891
Raphael ROY <raphael.roy@etudiant.univ-rennes1.fr>
2022-03-10 11:47:19 +0100

    premier commit

--- interaction.js
+++ interaction.js
@@ -2,14 +2,47 @@
 // La création d'un Dnd requière un canvas et un interacteur.
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
-	// Définir ici les attributs de la 'classe'
+	this.initX= 0;
+  this.initY=0;
+  this.endX=0;
+  this.endY=0;
+  this.active=false;
+
 
 	// Developper les 3 fonctions gérant les événements
 
+  //Fonction prenant en compte le clic de souris
+  DnD.prototype.Pression = function(evt){
+    this.initX=getMousePosition(canvas,evt).x;
+    this.initY=getMousePosition(canvas,evt).y;
+    console.log('x = '+this.initX + ', y = '+ this.initY);
+    this.active=true;
+  }
+
+  //Fonction prenant en compte le mouvement de la souris
+  DnD.prototype.Move = function(evt){
+    if (this.active = true){
+      this.endX=getMousePosition(canvas,evt).x;
+      this.endY=getMousePosition(canvas,evt).y;
+    }
+    console.log('x = '+this.initX+', y = '+this.initY);
+  }
+  //Fonction prenant en compte le relâchement du clic de souris
+  DnD.prototype.Releasing = function(evt) {
+    this.endX=getMousePosition(canvas,evt).x;
+    this.endY=getMousePosition(canvas,evt).y;
+    this.active=false;
+    console.log(' x = ' +this.endX + ', y = ' + this.endY)
+  }
+
+
 	// Associer les fonctions précédentes aux évènements du canvas.
+
+  canvas.addEventListener('mousedown', this.Pression, false);
+  canvas.addEventListener('mousemove', this.Move, false)
+  canvas.addEventListener('mouseup',this.Releasing, false)
+
 };
-
-
 // Place le point de l'événement evt relativement à la position du canvas.
 function getMousePosition(canvas, evt) {
   var rect = canvas.getBoundingClientRect();
--- model.js
+++ model.js
@@ -1,3 +1,53 @@
 
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
+function Drawing(){
+    this.formsArray=new Array; // Array contenant toutes les formes créées.
+
+}
+
+// Fonction ajoutant la forme donnée à la fin de notre Array.
+Drawing.prototype.addForms = function(form) {
+    this.formsArray.push(form);
+}
+
+// Fonction permettant de changer un élément de notre array si on modifie un tableau.
+Drawing.prototype.changeForm = function(id,form){
+    this.formsArray[id] = form;
+}
+
+// Fonction permettant de récupérer le tableau.
+Drawing.prototype.getForms = function(){
+    return this.formsArray;
+}
+
+// Fonction définissant la classe Forme de base. 
+function Forme(color, thickness){
+    this.color=color;
+    this.thickness=thickness;
+
+}
+
+// Fonction définissant la classe fille de Forme, Rectangle.
+// Le rectangle est défini à partir des éléments de base de form, ainsi que par son point en haut à gauche,
+// par sa hauteur et par sa largeur
+Forme.prototype.Rectangle = function(baseX,baseY, weight, height, thickness, color){
+
+    this.baseX=baseX;
+    this.baseY=baseY;
+    this.weight=weight;
+    this.height=height;
+    Forme.call(this, color, thickness);
+}
+
+// Fonction définissant la classe fille de Forme, Line.
+// La  ligne est définie à partir des éléments de base de form, ainsi que par les coordonnées de son point
+// de départ et de son point d'arrivée.
+Forme.prototype.Line = function(baseX, baseY, finalX, finalY, thickness, color){
+
+    this.baseX=baseX;
+    this.baseY=baseY;
+    this.finalX=finalX;
+    this.finalY=finalY;
+    Forme.call(this, color, thickness);
+};
