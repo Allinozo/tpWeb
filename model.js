cf9e578644361fe45d387c33821ddc9fdfc752f4
Raphael ROY <raphael.roy@etudiant.univ-rennes1.fr>
2022-03-10 13:15:06 +0100

    avancée au niveau de l'affichage, mais la forme ne change pas

--- model.js
+++ model.js
@@ -31,23 +31,74 @@ function Forme(color, thickness){
 // Fonction définissant la classe fille de Forme, Rectangle.
 // Le rectangle est défini à partir des éléments de base de form, ainsi que par son point en haut à gauche,
 // par sa hauteur et par sa largeur
-Forme.prototype.Rectangle = function(baseX,baseY, weight, height, thickness, color){
+function Rectangle(initX,initY, weight, height, thickness, color){
 
-    this.baseX=baseX;
-    this.baseY=baseY;
+    this.initX=initX;
+    this.initY=initY;
     this.weight=weight;
     this.height=height;
     Forme.call(this, color, thickness);
+
+}
+// Tous les getters pour la class Rectangle
+Rectangle.prototype.getInitX = function(){
+     return this.initX;
+}
+
+Rectangle.prototype.getInitY = function(){
+    return this.initY;
+}
+
+Rectangle.prototype.getWeight = function(){
+    return this.weight;
+}
+
+Rectangle.prototype.getHeight = function(){
+    return this.height;
+}
+
+Rectangle.prototype.getColor = function(){
+    return this.color;
+}
+
+Rectangle.prototype.getThickness = function(){
+    return this.thickness;
 }
 
 // Fonction définissant la classe fille de Forme, Line.
 // La  ligne est définie à partir des éléments de base de form, ainsi que par les coordonnées de son point
 // de départ et de son point d'arrivée.
-Forme.prototype.Line = function(baseX, baseY, finalX, finalY, thickness, color){
+function Line(baseX, baseY, finalX, finalY, thickness, color){
 
     this.baseX=baseX;
     this.baseY=baseY;
     this.finalX=finalX;
     this.finalY=finalY;
     Forme.call(this, color, thickness);
-};
+}    
+
+// Tous les getters pour la class Line
+
+Line.prototype.getInitX = function(){
+        return this.initX;
+}
+
+Line.prototype.getInitY = function(){
+    return this.initY;
+}
+
+Line.prototype.getFinalX = function(){
+    return this.finalX;
+}
+
+Line.prototype.getFinalY = function(){
+    return this.finalY;
+}
+
+Line.prototype.getColor = function(){
+    return this.color;
+}
+
+Line.prototype.getThickness = function(){
+    return this.thickness;
+}
--- view.js
+++ view.js
@@ -1,22 +1,25 @@
 
+
+
 // Implémenter ici les fonctions paint à ajouter dans chacune des classes du modèle.
 Rectangle.prototype.paint = function(ctx) {
     //TODO Manager color
     ctx.beginPath();
-    ctx.rect(this.getInitX(), this.getInitY(), this.getFinalX(),   this.getFinalY());
+    ctx.rect(this.getInitX(), this.getInitY(), this.getHeight(),   this.getWeight()); // Dessine le rectangle
+    //ctx.strokeStyle = this.getColor; //Rend la couleur de la forme créée.
     ctx.stroke();
   };
   
   Line.prototype.paint = function(ctx) {
     //TODO Manager color
     ctx.beginPath();
-    ctx.moveTo(this.getInitX(), this.getInitY());
-    ctx.lineTo(this.getFinalX(), this.getFinalY());
+    ctx.moveTo(this.getInitX(), this.getInitY());   //Setup le point de base de la ligne
+    ctx.lineTo(this.getFinalX(), this.getFinalY()); //Dessine la ligne entre (initX, initY) et (finalX,finalY)
     ctx.stroke();
   };
   
   Drawing.prototype.paint = function(ctx) {
-    //console.log(this.getForms());
+    console.log(this.getForms());
     ctx.fillStyle = '#F0F0F0'; // set canvas' background color
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     this.getForms().forEach(function (eltDuTableau) {
